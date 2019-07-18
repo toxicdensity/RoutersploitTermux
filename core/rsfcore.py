db8f89a9c9e81bc8ea374704f144f70751c87277
Mrcose <45144399+Mrcose@users.noreply.github.com>
2019-07-18 07:53:14 +0700

    Core

---
+++ core/rsfcore.py
@@ -0,0 +1,48 @@
+#!/data/data/com.termux/files/usr/bin/python
+
+import os
+import sys
+from time import sleep
+
+def runscript():
+	print("[*] Initializing...")
+	sleep(2)
+	os.system("cd $HOME")
+	os.system("termux-setup-storage")
+	sleep(1)
+
+	print("[*]Installing routersploit requirements with apt...")
+	sleep(2)
+	os.system("apt update && apt upgrade -y")
+	os.system("pkg upgrade && pkg install autoconf automake bison bzip2 clang cmake coreutils diffutils flex gawk git grep gzip libtool make patch perl sed silversearcher-ag tar wget pkg-config -y")
+	os.system("pkg install python-dev clang libcrypt-dev libffi-dev git openssl-dev -y")
+	sleep(1)
+
+	print("[*] Cloning routersploit...")
+	sleep(2)
+	os.system("git clone https://github.com/threat9/routersploit")
+	sleep(1)
+
+	print("[*] Installing requirements using python3-pip...")
+	sleep(2)
+	os.system("cd routersploit")
+	os.system("pip install --upgrade pip")
+	os.system("pip install requests")
+	os.system("python3 -m pip install -r requirements.txt")
+	os.system("python3 -m pip install -r requirements-dev.txt")
+	os.system("cd $HOME")
+	sleep(1)
+
+	print("[*] All installation is done. Clearing up installation script...")
+	sleep(2)
+	os.system("rm -rf $HOME/RoutersploitTermux")
+	os.system("rm -rf $HOME/run.sh")
+	sleep(1)
+	clear
+
+	print("[*] All done!")
+	sleep(2)
+	print("Run routersploit with command cd routersploit && python rsf.py")
+	print("")
+	print("Tools by Cose no copyright intended")
+	sleep(1)
